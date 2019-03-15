46de7664c5524fb798ce2a848443528b00e19744
iliaparvanov <ilia.nikolaev.parvanov@gmail.com>
2019-03-15 14:44:21 +0200

    Added code block with input

--- Magicode/Assets/VisualScripting/CodeBlockLoop.cs
+++ Magicode/Assets/VisualScripting/CodeBlockLoop.cs
@@ -1,24 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
-public class CodeBlockDerivative : CodeBlock
+public class CodeBlockLoop : CodeBlock
 {
-    private string var;
-
     public override void execute()
     {
         throw new System.NotImplementedException();
     }
 
+
     void Start()
     {
         this.executableCode = "Executable code for a generic code block";
-    }
-
-    public void setVar(TMP_InputField text)
-    {
-        this.var = text.text;
     }
 }
---
+++ Magicode/Assets/VisualScripting/GetTextFromField.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GetTextFromField : MonoBehaviour
+{
+
+    public TMP_InputField userInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        userInput.text = "123";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
