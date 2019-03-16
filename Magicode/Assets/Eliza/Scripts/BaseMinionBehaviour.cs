8d1768c345b31e2d90ba6624ee92503c603230a5
Gdemirev01 <gdemirev78@gmail.com>
2019-03-16 17:13:38 +0200

    StateManager(Broken)

--- Magicode/Assets/Eliza/Scripts/BaseMinionBehaviour.cs
+++ Magicode/Assets/Eliza/Scripts/BaseMinionBehaviour.cs
@@ -8,4 +8,14 @@ public class BaseMinionBehaviour : NetworkBehaviour {
     public Dictionary<string, Spell> spells;
     public GameObject bulletPrefab;
     [SyncVar] public float health;
-}
+    public static int minionsCounter = 6;
+
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            BaseMinionBehaviour.minionsCounter--;
+            //Destroy(this.gameObject);
+        }
+    }
+ }
---
+++ Magicode/Assets/GameStateManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.SceneManagement;
+
+public class GameStateManager : NetworkBehaviour
+{
+    public List<GameObject> minions = new List<GameObject>();
+    // Update is called once per frame
+    void Update()
+    {
+        if(BaseMinionBehaviour.minionsCounter <= 0)
+        {
+            Debug.Log("bye");
+            SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+        }
+    }
+}
