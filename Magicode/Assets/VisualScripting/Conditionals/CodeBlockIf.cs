8bbc6dd9b0e5bfaf1a1b88ad4738985a3e83684b
iliapa1 <ilia.nikolaev.parvanov@gmail.com>
2019-03-17 04:42:27 +0200

    Fixed if has collided

--- Magicode/Assets/VisualScripting/Conditionals/CodeBlockIf.cs
+++ Magicode/Assets/VisualScripting/Conditionals/CodeBlockIf.cs
@@ -5,50 +5,19 @@ using TMPro;
 
 public class CodeBlockIf : CodeBlockBegin
 {
-    [SerializeField]
-    private TMP_InputField leftArgumentInputField;
-
-    [SerializeField]
-    private TMP_InputField compareFunctionInputField;
-
-    [SerializeField]
-    private TMP_InputField rightArgumentInputField;
+    
 
     protected string condition = "true";
 
-    private bool getInput()
-    {
-        if (leftArgumentInputField != null && compareFunctionInputField != null && rightArgumentInputField != null)
-        {
-            float temp = 0;
-            if ((FindObjectOfType<TypeValidator>().validateFloat(leftArgumentInputField.text, out temp) ||
-                (CodeBlockCompiler.vars.ContainsKey(leftArgumentInputField.text) &&
-                FindObjectOfType<TypeValidator>().validateFloat(CodeBlockCompiler.vars[leftArgumentInputField.text], out temp)))
-                && 
-                (compareFunctionInputField.text == ">" || compareFunctionInputField.text == "<" || compareFunctionInputField.text == "==")
-                &&
-                (FindObjectOfType<TypeValidator>().validateFloat(rightArgumentInputField.text, out temp) ||
-                (CodeBlockCompiler.vars.ContainsKey(rightArgumentInputField.text) &&
-                FindObjectOfType<TypeValidator>().validateFloat(CodeBlockCompiler.vars[rightArgumentInputField.text], out temp))))
-            {
-                this.condition = leftArgumentInputField.text + compareFunctionInputField.text + rightArgumentInputField.text;
-                return true;
-            }
-        }
-        return false;
-    }
+    
 
     public override string execute()
     {
-        getInput();
-        //Debug.Log(condition);
         return "if(" + this.condition + ") {";
     }
 
     public override bool validateBlock()
     {
-        if (getInput())
-            return true;
-        return false;
+        return true;
     }
 }
---
+++ Magicode/Assets/VisualScripting/Conditionals/CodeBlockIfWithInputs.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CodeBlockIfWithInputs : CodeBlockIf
+{
+    [SerializeField]
+    private TMP_InputField leftArgumentInputField;
+
+    [SerializeField]
+    private TMP_InputField compareFunctionInputField;
+
+    [SerializeField]
+    private TMP_InputField rightArgumentInputField;
+
+    private bool getInput()
+    {
+        if (leftArgumentInputField != null && compareFunctionInputField != null && rightArgumentInputField != null)
+        {
+            float temp = 0;
+            if ((FindObjectOfType<TypeValidator>().validateFloat(leftArgumentInputField.text, out temp) ||
+                (CodeBlockCompiler.vars.ContainsKey(leftArgumentInputField.text) &&
+                FindObjectOfType<TypeValidator>().validateFloat(CodeBlockCompiler.vars[leftArgumentInputField.text], out temp)))
+                &&
+                (compareFunctionInputField.text == ">" || compareFunctionInputField.text == "<" || compareFunctionInputField.text == "==")
+                &&
+                (FindObjectOfType<TypeValidator>().validateFloat(rightArgumentInputField.text, out temp) ||
+                (CodeBlockCompiler.vars.ContainsKey(rightArgumentInputField.text) &&
+                FindObjectOfType<TypeValidator>().validateFloat(CodeBlockCompiler.vars[rightArgumentInputField.text], out temp))))
+            {
+                this.condition = leftArgumentInputField.text + compareFunctionInputField.text + rightArgumentInputField.text;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public override string execute()
+    {
+        getInput();
+        //Debug.Log(condition);
+        return "if(" + this.condition + ") {";
+    }
+
+    public override bool validateBlock()
+    {
+        if (getInput())
+            return true;
+        return false;
+    }
+
+}
